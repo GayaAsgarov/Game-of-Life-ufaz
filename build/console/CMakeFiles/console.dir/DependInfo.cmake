006e4d642561a605c4e54db026e8d8fa95ec4156
Gaya <qayaasgerov@gmail.com>
2022-06-11 23:57:37 +0400

    add main files

---
+++ build/console/CMakeFiles/console.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/apple/Desktop/my-game/game-of-life/board/board.c" "console/CMakeFiles/console.dir/__/board/board.c.o" "gcc" "console/CMakeFiles/console.dir/__/board/board.c.o.d"
+  "/Users/apple/Desktop/my-game/game-of-life/console/ansi.c" "console/CMakeFiles/console.dir/ansi.c.o" "gcc" "console/CMakeFiles/console.dir/ansi.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/apple/Desktop/my-game/game-of-life/build/board/CMakeFiles/board.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
